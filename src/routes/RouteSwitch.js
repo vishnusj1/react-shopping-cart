fa0df97a5f15ac74a601af5637a3b1662220d5d0
vishnusj <vishnusj12@gmail.com>
2022-04-08 15:48:36 +0530

    Initialise Routes

---
+++ src/routes/RouteSwitch.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "../App";
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={App} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default RouteSwitch;
