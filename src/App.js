3b54e96e5116e6f6198d43063fc9c0dc35965adf
vishnusj <vishnusj12@gmail.com>
2022-04-10 22:52:49 +0530

    Feature: Add Basic Cart functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./components/Cart";
+import Shop from "./routes/Shop";
+import HomePage from "./routes/HomePage";
+
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    const { id, image, title, price } = product;
+    if (cart.find((product) => product.id === id)) {
+      const newCart = cart.map((product) => {
+        if (product.id === id) {
+          return { ...product, qty: product.qty + 1 };
+        } else {
+          return product;
+        }
+      });
+      setCart([...newCart]);
+      return;
+    }
+    const newProduct = {
+      id,
+      image,
+      title,
+      price,
+      qty: 1,
+    };
+
+    setCart([...cart, newProduct]);
+  };
+
+  const removeFromCart = (id) => {
+    const productToRemove = cart.find((product) => product.id === id);
+    if (productToRemove.qty === 1) {
+      const Cart = cart.filter((product) => product.id !== productToRemove.id);
+      setCart([...Cart]);
+      return;
+    }
+    const NewCart = cart
+      .map((product) => {
+        if (product.id === id) {
+          return { ...product, qty: product.qty - 1 };
+        } else {
+          return product;
+        }
+      });
+    
+    setCart([...NewCart]);
+  };
+
+  const cartQuantity = cart.reduce((total, curItem) => total + curItem.qty, 0);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="shop" element={<Shop addToCart={addToCart} />} />
+      </Routes>
+      <div>Total Items in Cart : {cartQuantity}</div>
+      <Cart cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} />
     </div>
   );
 }
---
+++ src/components/Cart.js
@@ -0,0 +1,110 @@
+import { useEffect, useState } from "react";
+
+const Cart = ({ cart, addToCart, removeFromCart }) => {
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    const sumTotal = () => {
+      return cart
+        .reduce(
+          (total, curProduct) => total + curProduct.qty * curProduct.price,
+          0
+        )
+        .toFixed(2);
+    };
+    setCartTotal(sumTotal);
+  }, [cart]);
+
+  const itemTotal = (itemId) => {
+    const item = cart.find((item) => item.id === itemId);
+    const total = item.qty * item.price;
+    return total;
+  };
+
+  const itemsList = cart.map((item) => {
+    let total = itemTotal(item.id);
+    return (
+      <CartItem
+        key={item.id}
+        id={item.id}
+        title={item.title}
+        image={item.image}
+        price={item.price}
+        qty={item.qty}
+        itemTotal={total}
+        increment={addToCart}
+        decrement={removeFromCart}
+      />
+    );
+  });
+
+  return (
+    <div className="cartContainer">
+      <div className="cartHeader">
+        <h2>Your Cart</h2>
+      </div>
+      <div className="cartBody">
+        <div className="cartItems">
+          <ul>{itemsList}</ul>
+        </div>
+        {cart.length > 0 ? (
+          <div className="summary">
+            <div className="cartSummary">
+              <h2>Summary</h2>
+              <h3>Total : $ {cartTotal}</h3>
+            </div>
+          </div>
+        ) : (
+          <div className="cartEmpty">
+            <h3>No Items in your cart</h3>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const CartItem = ({
+  id,
+  title,
+  image,
+  price,
+  qty,
+  itemTotal,
+  decrement,
+  increment,
+}) => {
+  return (
+    <li className="cartItem">
+      <div className="itemTitle">{title}</div>
+      <div className="itemDetails">
+        <QuantityController
+          key={id}
+          id={id}
+          qty={qty}
+          increment={increment}
+          decrement={decrement}
+        />
+        <div className="itemTotal">
+          <span>$</span> {itemTotal}
+        </div>
+      </div>
+    </li>
+  );
+};
+
+const QuantityController = ({ id, qty, increment, decrement }) => {
+  return (
+    <div className="QtyController">
+      <div className="btn decrement" onClick={() => decrement(id)}>
+        -
+      </div>
+      <div>{qty}</div>
+      <div className="btn increment" onClick={() => increment({ id })}>
+        +
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
