d0c9f70c4844f0f087f0d33bba7d0db268c45276
vishnusj <vishnusj12@gmail.com>
2022-04-11 16:24:39 +0530

    Add App as root component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 import './index.css';
-import RouteSwitch from './routes/RouteSwitch'
 
 ReactDOM.render(
   <React.StrictMode>  
-  <RouteSwitch/>
+  <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
